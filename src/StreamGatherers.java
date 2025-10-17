9167900cdbd8d184ad99d5e1995a8e2d822b39bd
mahesh-sampadarao <mahesh.sampadarao@gmail.com>
2025-10-17 09:31:31 +0530

    Added StreamGatherers WindowSliding

--- src/StreamGatherers.java
+++ src/StreamGatherers.java
@@ -14,5 +14,11 @@ public class StreamGatherers {
         Stream<String> words = Stream.of("a", "b", "c", "d");
         List<String> resultList2 = words.gather(Gatherers.mapConcurrent(2, String::toUpperCase)).toList();
         IO.println(resultList2);
+
+        List<List<Integer>> expectedOutput = List.of(List.of(1, 2, 3), List.of(2, 3, 4), List.of(3, 4, 5));
+        Stream<Integer> numbers1 = Stream.of(1, 2, 3, 4, 5);
+        List<List<Integer>> resultList3 = numbers1.gather(Gatherers.windowSliding(3))
+                .toList();
+        IO.println(resultList3);
     }
 }
